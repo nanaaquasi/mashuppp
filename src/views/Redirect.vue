a7aedc121b58ad79ed3ff920e27c0b0ec9698e94
nanaaquasi <nanaaquasi5@gmail.com>
2021-11-11 21:52:32 +0000

    redirect page done

---
+++ src/views/Redirect.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { getParamValues } from "../utils";
+import { useRouter, useRoute } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+let hash = route.hash;
+
+const getAccessTokenFromUrl = async () => {
+  try {
+    const { access_token } = getParamValues(hash);
+
+    localStorage.setItem("access_token", access_token);
+
+    router.push("/welcome");
+  } catch (error) {
+    router.push("/");
+  }
+};
+
+getAccessTokenFromUrl();
+</script>
+<template>
+  <h1>Redirecting....</h1>
+</template>
